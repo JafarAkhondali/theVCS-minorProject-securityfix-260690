fea80802c388b28e6ce2566e751a0201c6d24894
Prince <princem.ec.19@nitj.ac.in>
2021-09-21 19:58:44 +0530

    added ace editor to `jsonConvertor`

--- jsonConvertor/urls.py
+++ jsonConvertor/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from jsonConvertor import views
 
 urlpatterns = [
-    path('',views.home,name='jsonConvertor')
+    path('',views.home,name='jsonConvertor'),
+    path('punctuationRemover/',views.punctuationRemover,name='punctuationRemover'),
 ]
--- jsonConvertor/views.py
+++ jsonConvertor/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import json
 
 # Create your views here.
+
+
 def home(request):
     context = {
         "title": "ML-TOOLBOX"
     }
     return render(request, 'jsonConvertor/home.html', context)
+
+
+def punctuationRemover(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+        for x in text.lower():
+            if x in punctuations:
+                text = text.replace(x, "")
+
+        return HttpResponse(json.dumps({"text": text}))
+    else:
+        return HttpResponse(json.dumps({"status": "failed"}))
