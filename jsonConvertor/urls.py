c8fa15ad4ccd8ee61696fd86a89e5c00a65bfb1e
nishant khatana <nishant.2020.khatana@gmail.com>
2021-09-22 21:33:24 +0530

    added all feature

--- jsonConvertor/urls.py
+++ jsonConvertor/urls.py
@@ -4,6 +4,9 @@ from jsonConvertor import views
 
 urlpatterns = [
     path('',views.home,name='jsonConvertor'),
-    path('punctuationRemover/',views.punctuationRemover,name='punctuationRemover'),
-    path('toupperconvertor/',views.toupperconvertor,name='toupperconvertor'),
+    path('PunctuationRemover/',views.PunctuationRemover,name='PunctuationRemover'),
+    path('ToUpperConvertor/',views.ToUpperConvertor,name='ToUpperConvertor'),
+    path('LineRemover/',views.LineRemover,name='LineRemover'),
+    path('ExtraSpaceRemover/',views.ExtraSpaceRemover,name='ExtraSpaceRemover'),
+    path('JSONconvertor/',views.JSONconvertor,name='JSONconvertor'),
 ]
--- jsonConvertor/views.py
+++ jsonConvertor/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import json
 import string 
-
 # Create your views here.
-
-
 def home(request):
     context = {
         "title": "ML-TOOLBOX"
@@ -12,37 +9,59 @@ def home(request):
     return render(request, 'jsonConvertor/home.html', context)
 
 
-def punctuationRemover(request):
+def PunctuationRemover(request):
     if request.method == "POST":
         intext= request.POST.get('text','no text enter')
-        
-        result=intext
-
-        analysed=""
-        for char in result:
+        result=""
+        for char in intext:
             if char not in string.punctuation:
-                analysed=analysed+char
-        result=analysed
-
-      
-        
+                result=result+char
         return HttpResponse(json.dumps({"result": result}))
     else:
         return HttpResponse(json.dumps({"status": "failed"}))
+        
 
-def toupperconvertor(request):
+def ToUpperConvertor(request):
     if request.method == "POST":
         intext= request.POST.get('text','no text enter')
-        
-        result=intext
-
-        analysed=""
-        analysed=analysed+intext.upper()
-        result="on"
-        result=analysed
-
-      
-        
+        result=""
+        result=result+intext.upper()
         return HttpResponse(json.dumps({"result": result}))
     else:
         return HttpResponse(json.dumps({"status": "failed"}))
+
+
+
+def LineRemover(request):
+    if request.method == "POST":
+        intext= request.POST.get('text','no text enter')        
+        result=""
+        for char in intext:
+            if char!="\n" and char!="\r":
+                result=result+char
+        return HttpResponse(json.dumps({"result": result}))
+    else:
+        return HttpResponse(json.dumps({"status": "failed"}))
+
+
+def ExtraSpaceRemover(request):
+    if request.method == "POST":
+        intext= request.POST.get('text','no text enter')        
+        result=""
+        for index,char in enumerate(intext):
+            if not(intext[index]==" "and intext[index+1]==" "):
+                result=result+char
+        return HttpResponse(json.dumps({"result": result}))
+    else:
+        return HttpResponse(json.dumps({"status": "failed"}))
+
+
+
+def JSONconvertor(request):
+    if request.method == "POST":
+        intext= request.POST.get('text','no text enter')        
+        result=json.dumps(intext)
+        return HttpResponse(json.dumps({"result": result}))
+    else:
+        return HttpResponse(json.dumps({"status": "failed"}))
+
