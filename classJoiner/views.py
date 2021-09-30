dc0ca2470fe8848764bbaed66c5ed4590396a862
Prince <princem.ec.19@nitj.ac.in>
2021-09-30 12:58:38 +0530

    classjoiner will send link to required persons

--- classJoiner/views.py
+++ classJoiner/views.py
@@ -42,8 +42,7 @@ def getclasses(request):
 
 def sendMessage(request):
     if request.method == 'POST':
-        df = pd.read_csv("./classJoiner/artifacts/phoneNo.csv")
-
+        # logging to the whatsapp account
         with open("./classJoiner/artifacts/secrets.json") as f:
             secret = json.load(f)
 
@@ -51,54 +50,29 @@ def sendMessage(request):
         auth_token = secret["auth_token"]
         client = Client(account_sid, auth_token)
 
+        # reading all the receivers
+        df = pd.read_csv("./classJoiner/artifacts/phoneNo.csv")
+
+        # loading time table
         with open("./classJoiner/artifacts/timetable.json") as f:
             _index = json.load(f)
 
+        # loading info about class timing
         day = request.POST.get('day')
         period = request.POST.get('period')
         period = int(float(period))
 
-        # getting link of all the classes
-        l1 = l2 = l3 = l4 = None
+        # sending link of all the classes
+        if period >= 9 or period < 0 or period == 5:
+            return HttpResponse(json.dumps({"status": "passed"}))
 
-        if period >= 9 or period < 0:
-            l1 = "No class now"
-            l2 = "No class now"
-            l3 = "No class now"
-            l4 = "No class now"
-        else:
-            # G1
-            if len(_index["G1"][day][period]) == 1:
-                l1 = "No class now"
-            else:
-                l1 = _index["G1"][day][period][2]
-
-            # G2
-            if len(_index["G2"][day][period]) == 1:
-                l2 = "No class now"
-            else:
-                l2 = _index["G2"][day][period][2]
-
-            # G3
-            if len(_index["G3"][day][period]) == 1:
-                l3 = "No class now"
-            else:
-                l3 = _index["G3"][day][period][2]
-
-            # G4
-            if len(_index["G4"][day][period]) == 1:
-                l4 = "No class now"
-            else:
-                l4 = _index["G4"][day][period][2]
-
-        text = "G1: {}\nG2: {}\nG3: {}\nG4: {}".format(l1, l2, l3, l4)
-
-        for phoneNo in df["phoneNo"]:
-            message = client.messages.create(
-                from_='whatsapp:+14155238886',
-                body=text,
-                to='whatsapp:+{}'.format(str(phoneNo))
-            )
+        for ind, row in df.iterrows():
+            if len(_index[row["group"]][day][period]) > 1:
+                message = client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body="Link: {}\nClass: {}\nTeacher: {}".format(_index[row["group"]][day][period][2], _index[row["group"]][day][period][1], _index[row["group"]][day][period][0]),
+                    to='whatsapp:+{}'.format(str(row["phoneNo"]))
+                )
 
         return HttpResponse(json.dumps({"status": "passed"}))
     else:
