6fc85ef3367356e97c9b91cc109d215a00e8d7b2
Prince <princem.ec.19@nitj.ac.in>
2021-10-03 19:52:55 +0530

    made some changes

--- minorProject/urls.py
+++ minorProject/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +16,4 @@ urlpatterns = [
     path('stockSentimentAnalysis/', include('stockSentimentAnalysis.urls')),
     path('classJoiner/', include('classJoiner.urls')),
     path('salaryPredictor/', include('salaryPredictor.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
